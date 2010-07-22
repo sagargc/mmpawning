f3ce2b83801f21366136abc44f5c84ce54b5ccc7
rasade88 <rasade88@94ab0e49-462b-1cf2-7d7b-ef8878dac214>
2010-07-22 17:50:32 +0000

    Fixed addition of branch variable of session, fixed login with md5 hashing.

--- trunk/MM/branch.php
+++ trunk/MM/branch.php
@@ -1,26 +1,22 @@
 <?php
 session_start();
-$day = date('d');
-$month = date('F');
-$numericMonth = date('m');
-$year = date('Y');
 $logged = $_SESSION['logged'];
 if ( $logged ) {
 ?>
 <fieldset>
 <legend><strong>Choose the Branch</strong></legend>
 <table>
-	<form method="post" action="">
+	<form method="post" action="home.php?page=branch&submitted=yes">
 		
 		<tr>
 			<td>Choose the Branch:</td>
 			<td>
-				<select>
-                    <option value="">Select</option>
-                    <option value="market">Anuradapura Market</option>
-					<option value="bus">Anuradapura Bus stand</option>
+				<select name="branch">
+                    <option value="" selected="selected">Select the branch</option>
+                    <option value="anu_market">Anuradapura Market</option>
+					<option value="anu_bus">Anuradapura Bus stand</option>
 					<option value="mm">MM Branch</option>
-					<option value="kurunagala">Kurunagala</option>
+					<option value="kurunegala">Kurunegala</option>
 					<option value="wariyapola">Wariyapola</option>
                     <option value="kekirawa">Kekirawa</option>
 					<option value="madawachchiya">Madawachchiya</option>                    
@@ -43,3 +39,8 @@ if ( $logged ) {
 <?php } else { ?>
 <p>You have not logged in. <a href="home.php?page=login">Click here to login again.</a></p>
 <?php } ?>
+<?php 
+if ( $_GET['submitted'] == "yes" ) {
+	$_SESSION['branch'] = $_POST['branch'];
+}
+?>
