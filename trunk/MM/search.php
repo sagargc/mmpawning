ea541f7fe83e5761135f8c82f11c94acf35caf48
rasade88 <rasade88@94ab0e49-462b-1cf2-7d7b-ef8878dac214>
2010-07-22 20:11:57 +0000

    Search done.

--- trunk/MM/search.php
+++ trunk/MM/search.php
@@ -38,7 +38,51 @@ if ( $logged ) {
 <?php } ?>
 <?php 
 if ( $_GET['submitted'] == 'yes' ) {
+	$display = '<frameset><legend>Details</legend><table width="80%" border="0">';
+				
 	if ( $_POST['type'] == 'cus_name' ) {
-		
+		$display .= '<tr>'
+					.'<th>Bill Id</th><th>Date</th><th>Weight</th>'
+					.'<th>Amount</th><th>Status</th><th>Branch</td></tr>';
+		$searchQuery = "SELECT cus_id FROM customer_details WHERE"
+						." name LIKE '%{$_POST['searchData']}'"
+						." OR name LIKE '{$_POST['searchData']}%'"
+						." OR name LIKE '%{$_POST['searchData']}%'";
+		$searchResult = mysql_query($searchQuery);
+		$cusId = mysql_result($searchResult, 'cus_id');
+		$transQuery = "SELECT ref_no FROM customer_ref WHERE"
+						." cus_id='$cusId'";
+		$transResult = mysql_query($transQuery);
+		$num = mysql_num_rows($transResult);
+		for ( $i = 0; $i < $num; $i++ ) {
+			$transId = mysql_result($transResult,$i,'ref_no');
+			$sql = "SELECT * FROM pawning WHERE ref_no='$transId'";
+			$data = mysql_query($sql);
+			$display .= '<tr><td>'.mysql_result($data,'ref_no').'</td>'
+						.'<td>'.mysql_result($data,'date').'</td>'
+						.'<td>'.mysql_result($data,'weight').'</td>'
+						.'<td>'.mysql_result($data,'amount').'</td>'
+						.'<td>'.mysql_result($data,'status').'</td>'
+						.'<td>'.mysql_result($data,'branch').'</td></tr>';
+		}
 	}
+	else if ( $_POST['type'] == 'bill_id' ) {
+		$display .= '<tr>'
+					.'<th>Customer Name</th><th>Date</th><th>Weight</th>'
+					.'<th>Amount</th><th>Status</th><th>Branch</td></tr>';
+		$sql2 = "SELECT * FROM pawning WHERE ref_no='{$_POST['searchData']}'";
+		$data = mysql_query($sql2);
+		$cusIdResult = mysql_query("SELECT cus_id FROM customer_ref WHERE ref_no'{$_POST['searchData']}'");
+		$cusId = mysql_result($cusIdResult,'cus_id');
+		$cusNameResult = mysql_query("SELECT name FROM customer_details WHERE cus_id='$cusId'");
+		$cusName = mysql_result($cusNameResult,'name');
+		$display .= '<tr><td>'.$cusName.'</td>'
+					.'<td>'.mysql_result($data,'date').'</td>'
+					.'<td>'.mysql_result($data,'weight').'</td>'
+					.'<td>'.mysql_result($data,'amount').'</td>'
+					.'<td>'.mysql_result($data,'status').'</td>'
+					.'<td>'.mysql_result($data,'branch').'</td></tr>';
+	}
+	$display .= '</table></frameset>';
+	echo $display;
 }
