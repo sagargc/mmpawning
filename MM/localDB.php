c11a87fcc851d7eeb73cf423a67003a44850c037
rasade88 <rasade88@94ab0e49-462b-1cf2-7d7b-ef8878dac214>
2010-08-01 16:47:47 +0000

    Added functions.

--- MM/localDB.php
+++ MM/localDB.php
@@ -6,6 +6,6 @@ $password = "";
 $username2 = "sandarau";
 $password2 = "5DXaugZ3xD";
 $database2 = "sandarau_mmp";
-mysql_connect(localhost,$username2,$password2);
-mysql_select_db($database2) or die("Unable to open database!");
+mysql_connect(localhost,$username,$password);
+mysql_select_db($database) or die("Unable to open database!");
 ?>
